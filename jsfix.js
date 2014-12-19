a17cd75681cfbe4aa6373d8834d348787cabd9e1
David Moreno <dmoreno@coralbits.com>
2014-12-19 16:38:48 +0100

    Added documentation, incuding example.

--- jsfix.js
+++ jsfix.js
@@ -1,8 +1,32 @@
 define(['jquery'],function($){
 	var d={}
-	d.__jsfix__={}
-	d.__jsfix__all__=""
+	d.__jsfix__={} // Map from fixes names to functions.
+	d.__jsfix__all__="" // Fast selector to look for all js-* at jsfix.fix call.
 	
+	/**
+	 * @short Register custom made js-tags.
+	 * 
+	 * Internally it uses this very same register to registry builtin js-tags, but new ones can be registered.
+	 * 
+	 * @param id Tag id, for example js-click
+	 * @param f Funtion to execute on each element that matches, with element as first argument (function($el){ ... })
+	 * 
+	 * Example of use:
+	 * 
+	 * 	jsfix.register('js-popup', function($el){
+	 * 		$el.click(function(){
+	 * 			require(['dialog'],function(dialog){
+	 * 				dialog.createOverWindow().load($el.attr('js-popup'))
+	 * 			})
+	 * 		})
+	 * 	})
+	 * 
+	 * This register a js-popup hook that opens overwindows (lightboxes, dialogs) with the given url.
+	 * 
+	 * The jsfix is applied inmediatly to the full page, so new entries work inmediatly.
+	 * 
+	 * @warning The js-* attribute is removed after the first call to the changing function, so if value must be used, it must be stored in a closure properly.
+	 */
 	d.register = function(name, f){
 		d.__jsfix__[name]=f
 		if (d.__jsfix__all__)
@@ -17,6 +41,21 @@ define(['jquery'],function($){
 	d.unregister = function(name){
 		delete d.__jsfix__[name]
 	}
+	/**
+		* @short Do the js-* fixes
+		* 
+		* It scans for elements into $root that have js-* attributes and link events as required. Known jsfixes:
+		* 
+		* Modules are set just by name, functions with name of module, dot, name of returned object function: mymodule.myfunc.
+		* 
+		* * js-load -- Loads the given module and runs the load method with the current element
+		* * js-click -- When click on event loads the modul and runs the function on the form "module.function"
+		* * js-popup -- At click, shows a popup with the given data.
+		* 
+		* Examples:
+		* 
+		*   <a href="#_" js-click="register.openDialog">Register</a>
+		*/
 	d.fix = function($el){
 		if ($(d.__jsfix__all__)){ // Gross search, should be faster than many small searches.
 			for (var i in d.__jsfix__){
@@ -28,9 +67,16 @@ define(['jquery'],function($){
 		}
 	}
 	
-	d.do = function(fix, $el){
+	/**
+	 * @short Do a jsfix on a given element.
+	 * 
+	 * Used internally, but can be used by other jsfixes to force some behaviours.
+	 */
+	d.do = function(fix, $el, data){
+		if (!data)
+			data=$el.attr(fix)
 		try{
-			d.__jsfix__[fix]($el)
+			d.__jsfix__[fix]($el, data)
 		}
 		catch(e){
 			console.error(e)
@@ -38,6 +84,12 @@ define(['jquery'],function($){
 		$el.removeAttr(fix)
 	}
 	
+	/**
+	 * @name js-require
+	 * @short Loads a requirejs module. 
+	 * 
+	 * If it has a load attribute it will be called.
+	 */
 	d.register('js-require', function($el, mod){
 		require([mod],function(a){
 			if (a.load)
